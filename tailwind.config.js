ff510cd6e01e7c124965683dfcdaa68dbf31acfb
Shihabun Mobin Jisan <shihabunjisan@gmail.com>
2023-06-18 17:14:49 +0600

    Somoy - Navbar, Hero, Features, Main Product find

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        midnightBlue: "#072a6c",
+        forestGreen: "#04724d",
+        floralWhite: "#fffcf2",
+        darkGray: "- rgb(51, 51, 51)",
+      },
+    },
+  },
+  plugins: [],
+};
